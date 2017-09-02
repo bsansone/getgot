1dff488087454d6ec1480dea848e790b653e7cd5
Brandon Sansone <sansonebrandon@gmail.com>
2017-09-02 11:41:47 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './GET_GOT.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="💩">
+          <img src={logo} className="😎" alt="GET GOT" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
